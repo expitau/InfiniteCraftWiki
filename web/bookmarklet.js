b50f9a215f01e4ff3c156e6b2cf9e0aa32c678eb
Reu Zuidema <22671898+reumarks@users.noreply.github.com>
2024-02-28 20:30:39 -0500

    Added user save data system

--- web/bookmarklet.js
+++ web/bookmarklet.js
@@ -1,1 +1,25 @@
-fetch("https://raw.githubusercontent.com/expitau/InfiniteCraftWiki/main/web/data/index.json").then(res => res.json()).then(data => { index = Object.fromEntries(Object.entries(data).map(x => [x[1][1], x[0]])); window.location.href = `https://expitau.github.io/InfiniteCraftWiki?s=${JSON.parse(localStorage.getItem('infinite-craft-data')).elements.map(a => index[a.text]).filter(x => x).join(",")}` })
+/* Click here to redirect to infinite craft  */
+function createIframeWithAccess() {
+   /* Create and append the Iframe */
+   var iframe = document.createElement('iframe');
+   iframe.style.display = 'none';
+   iframe.src = 'HELPER_URL';
+   document.body.appendChild(iframe);
+
+   /* Respond to requests for local storage from Iframe */
+   window.addEventListener('message', function (event) {
+      if (event.data === 'requestLocalStorage') {
+         var localStorageData = localStorage.getItem('infinite-craft-data');
+         event.source.postMessage(localStorageData, event.origin);
+      }
+   }, false);
+}
+
+/* Redirect the user to infinite craft if they are not there already */
+if (window.location.href != "https://neal.fun/infinite-craft/") {
+   window.location.href = "https://neal.fun/infinite-craft/";
+} else if (!top.hasOwnProperty('addOnCreated')) {
+   /* Append iframe is user is on infinite craft */
+   createIframeWithAccess();
+   top.addOnCreated = true;
+}
