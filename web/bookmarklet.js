78562e84fa1c669e0fd7e24509de5f084682ec96
Reu Zuidema <22671898+reumarks@users.noreply.github.com>
2024-03-02 02:23:31 -0500

    Added transition screens to bookmarklet

--- web/bookmarklet.js
+++ web/bookmarklet.js
@@ -15,11 +15,18 @@ function createIframeWithAccess() {
    }, false);
 }
 
+function displayMessage(header, content) {
+   document.body.innerHTML = `<div style="position: fixed;width: 100vw;text-align: center;height: 100vh;background-color: #191919;color: white;padding-top: 50px;font-size: 20px;font-family: "Roboto", sans-serif;"><div style=" font-weight: bold; font-size: larger;">${header}</div><br><div>${content}</div></div>`;
+}
+
 /* Redirect the user to infinite craft if they are not there already */
 if (window.location.href != "https://neal.fun/infinite-craft/") {
-   window.location.href = "https://neal.fun/infinite-craft/";
+   setTimeout(() => {
+      window.location.href = "https://neal.fun/infinite-craft/";
+   }, 1200);
+   displayMessage('Redirecting to Infinite Craft', 'Click this Bookmarklet again once you are there to save your data!');
 } else if (!top.hasOwnProperty('addOnCreated')) {
-   /* Append iframe is user is on infinite craft */
+   displayMessage('Redirecting to Infinite Craft Wiki', 'Do this any time to update your elements!');
    createIframeWithAccess();
    top.addOnCreated = true;
 }
